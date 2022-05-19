6d9e33fe28a3f345f570aa9ebd5179a913f9bdc4
Vraj Barot <vrajbarot@gmail.com>
2022-05-19 13:20:42 +0800

    Completed candidate detection script. Started working on candidate discrimination.

--- candidateDetection.m
+++ candidateDetection.m
@@ -1,11 +1,12 @@
+%% General Setup
+clear; clc;
 
-%% This is what has already happened:
 % Use parser to load folder
-fileLocation = 'C:\Users\vraj\OneDrive - The University of Western Australia\Uni\2022\Computer Vision\Project\VISO\mot\car\001';
+fileLocation = 'C:\Users\pc\OneDrive - The University of Western Australia\Uni\2022\Computer Vision\Project\VISO\mot\car\001';
 nameTemplate = '%06d';
-frameRange = [10 80];
+frameRange = [1 15];
 
-p = parser(fileLocation, nameTemplate, [10 20]) %frame range not specified because we want to load all images
+p = parser(fileLocation, nameTemplate, frameRange) %frame range not specified because we want to load all images
 
 % Load gt.txt data
 p_gt = p.read_csv();
@@ -17,8 +18,8 @@ p_gt = p.read_csv();
     for i = p.frameRange(1)+1:p.frameRange(2)-1
         % Load images at i-1, i and i+1
         img_1 = p.file_index(i).frame;
-        img_2 =p.file_index(i-1).frame;
-        img_3 =p.file_index(i+1).frame;
+        img_2 = p.file_index(i-1).frame;
+        img_3 = p.file_index(i+1).frame;
 
         % Convert Image to greyscale
         img_1_bw = rgb2gray(img_1);
@@ -44,7 +45,7 @@ p_gt = p.read_csv();
             % Number of rows = wholeBlockRows + 1
             % Number of cols = wholeBlockCols + 1
 
-        arr = cell(wholeBlockRows+1, wholeBlockCols+1); % creating empty array to input results into
+        arr{i-1} = cell(wholeBlockRows+1, wholeBlockCols+1); % creating empty array to input results into
 
         for row = 1:wholeBlockRows+1
             for col = 1:wholeBlockCols+1
@@ -66,13 +67,20 @@ p_gt = p.read_csv();
                 binary_13 = abs_diff_13 > threshold_13;
         
                 % Save binary frame to array
-                arr{row,col} = binary_12.*binary_13;
+                temp = arr{i-1};
+                temp{row,col} = binary_12 & binary_13;
+                arr{i-1} = temp;
             end
-            
-            % Display binary image
-            binary_image = cell2mat(arr);
-            imshow(binary_image);
-            drawnow;
-%             pause(0.05);
+
+            % Display binary image 
+          
+%             binary_image = cell2mat(arr);
+%             imshow(binary_image);
+%             drawnow;
+%             pause(0.01);
         end
+        
+        % Combine blocks into single matrix and place in arr
+        arr{i-1} = cell2mat(arr{i-1});
+
     end
---
+++ candidateDiscrimination.m
@@ -0,0 +1,5 @@
+bin = arr{1};
+
+[p_row, p_col] = find(bin);
+
+
